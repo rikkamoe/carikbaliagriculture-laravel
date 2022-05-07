807e9c73aba0d8d9a295d6a09249838d33e23793
RikkaMoe <bayuwidiastika@gmail.com>
2022-05-08 04:34:53 +0800

    Update History Order Customer

--- resources/views/customer/order.blade.php
+++ resources/views/customer/order.blade.php
@@ -51,7 +51,69 @@
                                             @endif
                                         </td>
                                         <td>Rp {{ number_format($item->total_order) }} untuk {{ $item->quantity_order }} items</td>
-                                        <td><a href="#" class="btn btn-dark"><span>View</span></a>
+                                        <td>
+                                            <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#staticBackdrop{{ $item->id_order }}">View</button>
+
+                                            <div class="modal fade" id="staticBackdrop{{ $item->id_order }}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                                <div class="modal-dialog modal-xl">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                        <h5 class="modal-title" id="staticBackdropLabel">Detail Order</h5>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            <div class="row">
+                                                                <hr>
+                                                                <h4>Information Order</h4>
+                                                                <hr>
+                                                                <div class="col-lg-6">
+                                                                    <div class="mb-3">
+                                                                        <label for="exampleFormControlInput1" class="form-label">ID Order</label>
+                                                                        <input type="text" value="{{  $item->id_order }}" class="form-control" readonly>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="col-lg-6">
+                                                                    <div class="mb-3">
+                                                                        <label for="exampleFormControlTextarea1" class="form-label">Date Order</label>
+                                                                        <input type="text" class="form-control" value="{{ $item->date_order }}" readonly>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="col-lg-4">
+                                                                    <div class="mb-3">
+                                                                        <label for="exampleFormControlInput1" class="form-label">Product Order</label>
+                                                                        <input type="text" value="{{  $item->name_product }}" class="form-control" readonly>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="col-lg-4">
+                                                                    <div class="mb-3">
+                                                                        <label for="exampleFormControlInput1" class="form-label">Quantity Order</label>
+                                                                        <input type="text" value="{{  $item->quantity_order }}" class="form-control" readonly>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="col-lg-4">
+                                                                    <div class="mb-3">
+                                                                        <label for="exampleFormControlInput1" class="form-label">Total Order</label>
+                                                                        <input type="text" value="Rp. {{  number_format($item->total_order) }}" class="form-control" readonly>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="col-lg-12">
+                                                                    <div class="mb-3">
+                                                                        <label for="exampleFormControlTextarea1" class="form-label">Keterangan Order</label>
+                                                                        <textarea class="form-control" rows="3" readonly>@if ($item->status_order == '0') Pesanan Dikemas @elseif ($item->status_order == '1') Pesanan Dikirim @elseif ($item->status_order == '2') Pesanan Selesai @else Pesanan Dibatalkan @endif : {{ $item->description_order }}</textarea>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </td>
                                     </tr>
                                     @endforeach
